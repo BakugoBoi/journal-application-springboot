025805fbba338068911ec4b20d5d6dd4598dc9eb
Mahendra Markande <79150653+BakugoBoi@users.noreply.github.com>
2024-06-08 21:16:13 +0530

    Delete banking-app directory

--- banking-app/src/main/java/com/bank/banking/repo/AccountRepo.java
+++
@@ -1,7 +0,0 @@
-package com.bank.banking.repo;
-
-import com.bank.banking.entity.AccountEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepo extends JpaRepository<AccountEntity,Long> {
-}
